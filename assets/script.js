eb4c2a9d08fd10861bbb09cd6d3a315bd800ae63
BastienD <biloute.bd@gmail.com>
2023-10-02 11:38:58 +0200

    boucle fonctionelle

--- assets/script.js
+++ assets/script.js
@@ -28,11 +28,15 @@ let dotSelector1 = document.getElementById("dot1");
 let dotSelector2 = document.getElementById("dot2");
 let dotSelector3 = document.getElementById("dot3");
 
-function counterVariable (){
-	if (counter > 3){
-		counter = 0
+function variableCheck (){
+	switch (counter){
+		case 4:
+			counter = 0
+			break
+		case -1:
+			counter = 3
+			break
 	}
-	return counter;
 }
 
 function updateBannerContent(){
@@ -48,23 +52,23 @@ function dotRemover(){
 }
 
 
-function listenCounter(){
+function upadteDots(){
 	dotRemover();
 	switch (counter) {
 		case 0:
-			console.log("numero 0")
+			console.log("numero 0");
 			dotSelector0.classList = "dot dot_selected";
 			break
 		case 1:
-			console.log("numero 1")
+			console.log("numero 1");
 			dotSelector1.classList = "dot dot_selected";
 			break
 		case 2:
-			console.log("numero 2")
+			console.log("numero 2");
 			dotSelector2.classList = "dot dot_selected";
 			break
 		case 3:
-			console.log("numero 3")
+			console.log("numero 3");
 			dotSelector3.classList = "dot dot_selected";
 			break
 	}
@@ -73,45 +77,47 @@ function listenCounter(){
 
 
 
-listenCounter();
+upadteDots();
 
 rightSwipe.addEventListener("click", function(){
 	counter += 1;
-	updateBannerContent()
-	listenCounter()
-	console.log(counter)
+	variableCheck()
+	updateBannerContent();
+	upadteDots();
+	console.log(counter);
 })
 
 leftSwipe.addEventListener("click", function(){
 	counter -= 1;
-	updateBannerContent()
-	listenCounter()
-	console.log(counter)
+	variableCheck()
+	updateBannerContent();
+	upadteDots();
+	console.log(counter);
 })
 
 dotSelector0.addEventListener("click", function(){
-	counter = 0
-	listenCounter();
+	counter = 0;
+	upadteDots();
 	updateBannerContent();
-	console.log(counter)
+	console.log(counter);
 })
 dotSelector1.addEventListener("click", function(){
-	counter = 1
-	listenCounter();
+	counter = 1;
+	upadteDots();
 	updateBannerContent();
-	console.log(counter)
+	console.log(counter);
 })
 dotSelector2.addEventListener("click", function(){
-	counter = 2
-	listenCounter();
+	counter = 2;
+	upadteDots();
 	updateBannerContent();
-	console.log(counter)
+	console.log(counter);
 })
 dotSelector3.addEventListener("click", function(){
-	counter = 3
-	listenCounter();
+	counter = 3;
+	upadteDots();
 	updateBannerContent();
-	console.log(counter)
+	console.log(counter);
 })
 
 
